7e0a61e914017579c4f9b36390b438b961f42d79
Alexey Ryzhkov <wertybded@gmail.com>
2025-04-14 21:04:04 +0300

    Program

--- ConsoleLesson5/ConsoleLesson5/Program.cs
+++ ConsoleLesson5/ConsoleLesson5/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("This is lesson 5");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> list = new List<int>();
+        int i = 200;
+        while (true)
+        {
+            if(i%17 == 0)
+            {
+                list.Add(i);
+            }
+            if(i>500 ) break;
+            i++;
+        }
+        Console.WriteLine(string.Join(", ", list));
+    }
+}
